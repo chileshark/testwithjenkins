a7e274af0d726e8b01027559477594353dadbaf1
Lucas431 <60723467+Lucas431@users.noreply.github.com>
2021-05-08 14:56:45 +0800

    Update testwithjenkinsfromshell.sh

--- testwithjenkinsfromshell.sh
+++ testwithjenkinsfromshell.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 yum -y install httpd
-echo "This is test with jenkins !" > /var/www/html/index.html
-systemctl enable --now httpds
+echo 'This is test with jenkins' > /var/www/html/index.html
+systemctl enable --now httpd
